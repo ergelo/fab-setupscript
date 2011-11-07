d84c4e735d8ee75862c177abd6c2ad442ddfbd01
Bruno Panara <bruno.panara@gmail.com>
2011-11-07 13:00:20 +0000

    added db nuking funcs and tested

--- deployment_scripts/fabfile.py
+++ deployment_scripts/fabfile.py
@@ -8,6 +8,7 @@ import getpass
 ################
 
 env.hosts = ['']
+project_directory = ''
 username = ''
 password = '' #optional
 
@@ -25,7 +26,7 @@ def deploy():
 
 #pushes branch to github and merges with development
 def close_branch(branch):
-    if confirm('Merging "%s" in "development", deleting and pushing?'):
+    if confirm('Merging "%s" in "development", deleting and pushing (did you commit)?' % branch):
         user()
         local('git push origin %s' % branch)
         local('git checkout development')
@@ -38,6 +39,15 @@ def new_branch(branch):
     user()
     local('git checkout -b %s development' % branch)
     local('git push origin %s' % branch)
+
+#nukes the db, creates a new one and runs syncdb, optionally south migrate as well
+def reset_db(dbname, south=False):
+    user()
+    nuke_db(dbname)
+    with cd('/home/web/%s/' % project_directory):
+        run('django-admin.py syncdb')
+        if south:
+            run ('django-admin.py migrate')
 
 ################
 #    helpers   #
@@ -61,7 +71,7 @@ def nginx():
 #pull new development code
 def pull():
     user()
-    with cd('home/web/%s/' % project_directory):
+    with cd('/home/web/%s/' % project_directory):
         run('git checkout development')
         run('git pull origin development')
 
@@ -69,16 +79,8 @@ def pull():
 def push():
     local('git push origin development')
 
-#pushes branch to github and merges with development
-def close_branch(branch):
-    if confirm('Merging "%s" in "development", deleting and pushing?'):
-        local('git push origin %s' % branch)
-        local('git checkout development')
-        local('git merge --no-ff %s' % branch)
-        local('git branch -d %s' % branch)
-        push()
-
-#creates a new branch off "development"
-def new_branch(branch):
-    local('git checkout -b %s development' % branch)
-    local('git push origin %s' % branch)
+#delete the db and create a new one with the same name
+def nuke_db(dbname):
+    user()
+    run("dropdb %s" % dbname)
+    run("createdb -O %s %s" % (username, dbname))
